8723b809aa2648d1cf718c020773c2247549ba99
john <john@primitive.io>
2024-03-21 14:57:24 -0700

    v2

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # NewickParser
+
+A parser for Newick .nwk timetree graphs.
+
+Translated from: https://gist.github.com/Ad115/34dfc6560b64779a40c1a929f560511b
---
+++ TreeNode.cs
@@ -0,0 +1,24 @@
+﻿namespace NewickParser;
+
+public class TreeNode(string label, List<TreeNode> children, float? distance, string features)
+{
+    public string Label { get; } = label;
+    public List<TreeNode> Children { get; } = children;
+    public float? Distance { get; } = distance;
+    public string Features { get; } = features;
+
+    public override string ToString()
+    {
+        return Label;
+    }
+
+    public void Print(int depth = 0)
+    {
+        Console.WriteLine($"{new string(' ', depth)}{Label} {Distance} {Features}");
+
+        foreach (TreeNode child in Children)
+        {
+            child.Print(depth + 1);
+        }
+    }
+}
