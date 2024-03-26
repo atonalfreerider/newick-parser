2a63f799ec8175713be3f6854c4676da5be7d93e
john <john@primitive.io>
2024-03-25 17:22:38 -0700

    parent references

--- TreeNode.cs
+++ TreeNode.cs
@@ -1,11 +1,41 @@
 ﻿namespace NewickParser;
 
-public class TreeNode(string label, List<TreeNode> children, float? distance, string features)
+public class TreeNode
 {
-    public string Label { get; } = label;
-    public List<TreeNode> Children { get; } = children;
-    public float? Distance { get; } = distance;
-    public string Features { get; } = features;
+    public TreeNode(string label, List<TreeNode> children, float? distance, string features)
+    {
+        Label = label;
+        Children = children;
+        Distance = distance;
+        Features = features;
+
+        foreach (TreeNode treeNode in children)
+        {
+            treeNode.Parent = this;
+        }
+    }
+
+    public string Label { get; }
+    public List<TreeNode> Children { get; }
+    public float? Distance { get; }
+    public string Features { get; }
+    
+    public TreeNode? Parent { get; private set; }
+
+    public float DistanceToRoot()
+    {
+        if (Parent == null)
+        {
+            return Distance ?? 0;
+        }
+        
+        return (Distance ?? 0) + Parent.DistanceToRoot();
+    }
+
+    public float BranchDistanceToRoot()
+    {
+        return DistanceToRoot() - (Distance ?? 0);
+    }
 
     public override string ToString()
     {
@@ -20,5 +50,25 @@ public class TreeNode(string label, List<TreeNode> children, float? distance, st
         {
             child.Print(depth + 1);
         }
+    }
+
+
+    public TreeNode? FindChild(string childName)
+    {
+        foreach (TreeNode child in Children)
+        {
+            if (child.Label == childName)
+            {
+                return child;
+            }
+            
+            TreeNode? foundChild = child.FindChild(childName);
+            if (foundChild != null)
+            {
+                return foundChild;
+            }
+        }
+
+        return null;
     }
 }
